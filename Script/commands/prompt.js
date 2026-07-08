155b7ed1a21138fa4f23967dc173fe3fb9335db5
Shamiul Haque <xg36006@gmail.com>
2026-07-08 20:34:17 +0600

    Update error message to include new name

--- Script/commands/prompt.js
+++ Script/commands/prompt.js
@@ -73,7 +73,7 @@ module.exports.run = async function ({ api, event, args }) {
 
   } catch (err) {
     return api.sendMessage(
-      "API Error call Boss SAHU 😣: " + err.message,
+      "API Error call Boss Šãmîuł Hãquè 😣: " + err.message,
       event.threadID,
       event.messageID
     );
