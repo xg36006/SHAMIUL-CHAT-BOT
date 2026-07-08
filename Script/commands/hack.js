67a8e32781d76e91cb71458ff42f89bcfbaf1855
Shamiul Haque <xg36006@gmail.com>
2026-07-08 20:09:44 +0600

    Update hack.js

--- Script/commands/hack.js
+++ Script/commands/hack.js
@@ -66,6 +66,6 @@ module.exports.run = async function ({ event, api }) {
     );
 
   } catch (e) {
-    return api.sendMessage("API Error Call Boss SAHU", threadID, messageID);
+    return api.sendMessage("API Error Call Boss Šãmîuł Hãquè", threadID, messageID);
   }
 };
