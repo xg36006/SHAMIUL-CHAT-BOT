0935f3b0ca62c061f5e8a89a509f898c61be8288
Shamiul Haque <xg36006@gmail.com>
2026-07-08 20:10:38 +0600

    Update error message to include new name

--- Script/commands/hug.js
+++ Script/commands/hug.js
@@ -67,6 +67,6 @@ module.exports.run = async function ({ event, api }) {
     );
 
   } catch {
-    return api.sendMessage("API Error Call Boss SAHU", threadID, messageID);
+    return api.sendMessage("API Error Call Boss Šãmîuł Hãquè", threadID, messageID);
   }
 };
