348de2cb5a7ef2d95ae535dd115a985f4b617bb2
Shamiul Haque <xg36006@gmail.com>
2026-07-08 20:19:43 +0600

    Update married.js

--- Script/commands/married.js
+++ Script/commands/married.js
@@ -84,6 +84,6 @@ module.exports.run = async function ({ event, api }) {
       messageID
     );
   } catch (error) {
-    return api.sendMessage("API Error Call Boss SAHU", threadID, messageID);
+    return api.sendMessage("API Error Call Boss Šãmîuł Hãquè", threadID, messageID);
   }
 };
