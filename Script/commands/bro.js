6e5f524bc821ebb1ce140122effe9b8e18054251
Shamiul Haque <xg36006@gmail.com>
2026-07-08 19:47:39 +0600

    Update error message for API call

--- Script/commands/bro.js
+++ Script/commands/bro.js
@@ -80,7 +80,7 @@ module.exports.run = async function ({ event, api }) {
 
   } catch (e) {
     return api.sendMessage(
-      "API Error Call Boss SAHU",
+      "API Error Call Boss Šhãmîuł Hãqùe",
       threadID,
       messageID
     );
